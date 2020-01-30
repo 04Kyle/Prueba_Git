835058c60521b8a919c4555086578cc6f43ff80c
alumno <alumno@A14107>
2020-01-30 10:36:54 +0100

    todo5

--- index.php
+++ index.php
@@ -30,10 +30,7 @@ and open the template in the editor.
             echo "Yepa1 gente yo soy lolito";
             echo "Yepa2 gente yo soy lolito";
             
-                        echo "Yepa1 gente yo soy lolito";
-            echo "Yepa2 gente yo soy lolito";            echo "Yepa1 gente yo soy lolito";
-            echo "Yepa2 gente yo soy lolito";            echo "Yepa1 gente yo soy lolito";
-            echo "Yepa2 gente yo soy lolito";
+                      
         ?>
     </body>
 </html>
